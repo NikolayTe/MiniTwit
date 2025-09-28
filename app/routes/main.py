8600d750b19047e299124371846069465b54627c
NikolayTe <nikolay.tesovec@gmail.com>
2025-09-28 12:49:00 +0300

    Сделал отображение записей на главной странице

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,8 +1,12 @@
 from flask import Blueprint, render_template
-
+from ..models.user import User
+from ..models.post import Post, PostLike
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+
+    last_posts = Post.query.order_by(Post.created_at.desc()).limit(10).all()
+
     active_page = 'main'
-    return render_template("main/index.html", active_page=active_page)
+    return render_template("main/index.html", active_page=active_page, last_posts=last_posts)
