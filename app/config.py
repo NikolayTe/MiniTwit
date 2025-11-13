3ffadb587d38a9dabaa66ef329b3c11cbc556e26
NikolayTe <nikolay.tesovec@gmail.com>
2025-11-14 00:31:20 +0300

    add utf-8 SQLALCHEMY_DATABASE_URI

--- app/config.py
+++ app/config.py
@@ -17,7 +17,7 @@ class Config(object):
     PORT = os.environ.get('POSTGRES_PORT', 5532)
     DB = os.environ.get('POSTGRES_DB', 'dbtwit')
 
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}?client_encoding=utf8"
     SECRET_KEY = 'SDFfrf234vfM23fBfs234KD9fsdsef2'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
 
